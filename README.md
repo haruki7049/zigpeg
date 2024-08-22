6fb51ced33fdaa39d445b1511ef974ecd95e82aa
haruki7049 <tontonkirikiri@gmail.com>
2024-08-22 20:01:46 +0900

    chore(repo): Init zig project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # zigpeg
 PEG parser generator by ziglang
+
+# Supported zig version
+- 0.13.0
---
+++ treefmt.nix
@@ -0,0 +1,6 @@
+{
+  projectRootFile = "flake.nix";
+  programs.nixpkgs-fmt.enable = true;
+  programs.zig.enable = true;
+  programs.actionlint.enable = true;
+}
