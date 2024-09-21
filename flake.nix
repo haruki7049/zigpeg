80bbd90df563be8bd7664401c96ec1518d612c78
haruki7049 <tontonkirikiri@gmail.com>
2024-09-21 20:10:36 +0900

    chore(flake.nix): Change flake-utils to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,62 +1,70 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, treefmt-nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem =
+        { pkgs, ... }:
+        let
+          zig = pkgs.zig_0_13;
+          zigpeg = pkgs.stdenv.mkDerivation {
+            pname = "zigpeg";
+            version = "dev";
+
+            src = ./.;
+
+            nativeBuildInputs = [
+              zig.hook
+            ];
+
+            zigBuildFlags = [
+              "-Doptimize=Debug"
+            ];
+          };
+        in
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.zig.enable = true;
+            programs.actionlint.enable = true;
+          };
+
+          checks = {
+            inherit zigpeg;
+          };
+
+          packages = {
+            inherit zigpeg;
+            default = zigpeg;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              # Compiler
+              zig
+
+              # LSP
+              pkgs.zls
+              pkgs.nil
+            ];
+
+            shellHook = ''
+              export PS1="\n[nix-shell:\w]$ "
+            '';
+          };
         };
-        stdenv = pkgs.stdenv;
-        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-        zigpeg = stdenv.mkDerivation {
-          pname = "zigpeg";
-          version = "dev";
-
-          src = ./.;
-
-          nativeBuildInputs = [
-            pkgs.zig_0_13.hook
-          ];
-
-          zigBuildFlags = [
-            "-Doptimize=Debug"
-          ];
-        };
-      in
-      {
-        # Use `nix fmt`
-        formatter = treefmtEval.config.build.wrapper;
-
-        # Use `nix flake check`
-        checks = {
-          inherit zigpeg;
-          formatting = treefmtEval.config.build.check self;
-        };
-
-        # nix build .
-        packages = {
-          inherit zigpeg;
-          default = zigpeg;
-        };
-
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            # Compiler
-            zig_0_13
-
-            # LSP
-            zls
-            nil
-          ];
-
-          shellHook = ''
-            export PS1="\n[nix-shell:\w]$ "
-          '';
-        };
-      });
+    };
 }
