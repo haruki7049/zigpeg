342cd01a7dfb92e7a6d8aaa0ea7165a722cf25c5
haruki7049 <tontonkirikiri@gmail.com>
2026-06-26 15:51:52 +0900

    Update

--- flake.nix
+++ flake.nix
@@ -98,8 +98,6 @@
               # Remove NIX_CFLAGS_COMPILE because zig cannot understand it
               unset NIX_CFLAGS_COMPILE
             '';
-
-            # inputsFrom = [ config.treefmt.build.devShell ];
           };
         };
     };
